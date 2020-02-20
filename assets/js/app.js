7cc73397be4cb06ed3b7c78030f45f8426f8714c
Skylar Kelty <skylar.kelty@kcl.ac.uk>
2020-02-20 13:22:49 +0000

    Tweak sidebar page

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,22 @@
+import 'jquery';
+import 'jquery-ui';
 import 'bootstrap';
 import '@fortawesome/fontawesome-free/js/all';
+
+window.$ = window.jQuery = require('jquery');
+
+$(document).on('scroll', function () {
+  var scrollDistance = $(this).scrollTop();
+  if (scrollDistance > 100) {
+    $('.scroll-to-top').fadeIn();
+  } else {
+    $('.scroll-to-top').fadeOut();
+  }
+});
+
+$(document).on('click', 'a.scroll-to-top', function (e) {
+  $('html, body').stop().animate({
+    scrollTop: 0
+  }, 1000, 'swing');
+  e.preventDefault();
+});
