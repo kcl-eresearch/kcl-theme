62ec62b36d41df7d00e3a2a0d755031fd6ff46ec
Skylar Kelty <skylar.kelty@kcl.ac.uk>
2020-02-20 15:23:02 +0000

    Fine tuning

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,20 +3,43 @@ import 'jquery-ui';
 import 'bootstrap';
 import '@fortawesome/fontawesome-free/js/all';
 
-window.$ = window.jQuery = require('jquery');
+require(['jquery', 'bootstrap'], function($, b) {
+  window.$ = window.jQuery = $;
 
-$(document).on('scroll', function () {
-  var scrollDistance = $(this).scrollTop();
-  if (scrollDistance > 100) {
-    $('.scroll-to-top').fadeIn();
-  } else {
-    $('.scroll-to-top').fadeOut();
+  $.urlParam = function(name){
+  	var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+      if (results && results.length > 0) {
+      	return results[1];
+      }
+
+      return 0;
   }
-});
 
-$(document).on('click', 'a.scroll-to-top', function (e) {
-  $('html, body').stop().animate({
-    scrollTop: 0
-  }, 1000, 'swing');
-  e.preventDefault();
-});
+  function setupPage() {
+    $('[data-toggle="hoverover"]').popover({
+        trigger: 'hover'
+    });
+
+    $('[data-toggle="popover"]').popover({
+        trigger: 'focus'
+    });
+  }
+
+  setupPage();
+
+  $(document).on('scroll', function () {
+    var scrollDistance = $(this).scrollTop();
+    if (scrollDistance > 100) {
+      $('.scroll-to-top').fadeIn();
+    } else {
+      $('.scroll-to-top').fadeOut();
+    }
+  });
+
+  $(document).on('click', 'a.scroll-to-top', function (e) {
+    $('html, body').stop().animate({
+      scrollTop: 0
+    }, 1000, 'swing');
+    e.preventDefault();
+  });
+}
