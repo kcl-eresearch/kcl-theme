0dc40a587476193975fcd14ddda3dc35a58b58eb
Skylar Kelty <skylar.kelty@kcl.ac.uk>
2020-03-31 11:19:47 +0100

    Rebuild prod

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -8,6 +8,15 @@ global.Bloodhound = Bloodhound;
   require('jquery-ui')
   require('bootstrap')
   require('dropzone')
+  window.dt = require('datatables.net-bs4');
+  require('datatables.net-responsive-bs4');
+  require('select2')
+  require('bootstrap4-duallistbox')
+  require('node-waves')
+  require('jquery-slimscroll')
+  require('typeahead.js/dist/typeahead.jquery')
+  global.ClipboardJS = require('clipboard')
+  global.Quill = require('quill')
 
   $.urlParam = function(name){
   	var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
@@ -18,6 +27,7 @@ global.Bloodhound = Bloodhound;
       return 0;
   }
 
+  // Utils.
   window.setupNMSPage = function() {
     $('[data-toggle="hoverover"]').popover({
         trigger: 'hover',
@@ -38,63 +48,87 @@ global.Bloodhound = Bloodhound;
     }, 10000);
   }
 
-  window.setupNMSPage();
+  // Actual document ready event.
+  $(document).ready(function() {
+    // Quill.
+    if ($('.quill-editor').length > 0) {
+      var editor = new Quill('.quill-editor', {
+        modules: {
+          toolbar: [
+            ['bold', 'italic', 'underline', 'strike'],
+            ['blockquote', 'code-block'],
 
-  // DataTables.
-  window.dt = require('datatables.net-bs4');
-  require('datatables.net-responsive-bs4');
-  $('.datatable').DataTable();
+            [{ 'header': 1 }, { 'header': 2 }],
+            [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+            [{ 'script': 'sub'}, { 'script': 'super' }],
+            [{ 'indent': '-1'}, { 'indent': '+1' }],
+            [{ 'direction': 'rtl' }],
 
-  // Select 2.
-  require('select2')
-  $('select:not(.nos2):not(.duallistbox)').select2({'theme': 'bootstrap4'});
+            [{ 'size': ['small', false, 'large', 'huge'] }],
+            [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
 
-  // Dual List-Box.
-  require('bootstrap4-duallistbox')
-  $('.duallistbox').bootstrapDualListbox();
+            [{ 'color': [] }, { 'background': [] }],
+            [{ 'font': [] }],
+            [{ 'align': [] }],
 
-  // Waves.
-  require('node-waves')
-  if (window.Waves) {
+            ['clean']
+          ],
+          history: {
+            delay: 2000,
+            maxStack: 500,
+            userOnly: true
+          }
+        },
+        theme: 'snow'
+      });
+    }
+
+    // DataTables.
+    $('.datatable').DataTable();
+
+    // Clipboard.JS.
+    if ($('.btn-clipboard').length > 0) {
+      var clipboard = new ClipboardJS('.btn-clipboard');
+    }
+
+    // Select 2.
+    $('select:not(.nos2):not(.duallistbox):visible').select2({'theme': 'bootstrap4'});
+
+    // Dual List-Box.
+    $('.duallistbox').bootstrapDualListbox();
+
+    // Slimscroll.
+    $('.slimscroll').slimScroll({
+      position: 'right',
+      color: '#0A2D50',
+      railVisible: true,
+      alwaysVisible: true
+    });
+
+    // Waves.
     var wavesSelect = '.nav-menu:not(.js-waves-off) a, .btn:not(.js-waves-off):not(.btn-switch), .js-waves-on';
     if ($(wavesSelect).length > 0) {
       Waves.attach(wavesSelect, ['waves-themed']);
       Waves.init();
     }
-  }
 
-  // Slimscroll.
-  require('jquery-slimscroll')
-  $('.slimscroll').slimScroll({
-  	position: 'right',
-    color: '#0A2D50',
-  	railVisible: true,
-  	alwaysVisible: true
-  });
+    // Utils.
+    $(document).on('scroll', function () {
+      var scrollDistance = $(this).scrollTop();
+      if (scrollDistance > 100) {
+        $('.scroll-to-top').fadeIn();
+      } else {
+        $('.scroll-to-top').fadeOut();
+      }
+    });
 
-  // Clipboard.JS.
-  var ClipboardJS = require('clipboard')
-  if ($('.btn-clipboard').length > 0) {
-    var clipboard = new ClipboardJS('.btn-clipboard');
-  }
+    $(document).on('click', 'a.scroll-to-top', function (e) {
+      $('html, body').stop().animate({
+        scrollTop: 0
+      }, 1000, 'swing');
+      e.preventDefault();
+    });
 
-  // Typeahead.js.
-  require('typeahead.js/dist/typeahead.jquery')
-
-  // Utils.
-  $(document).on('scroll', function () {
-    var scrollDistance = $(this).scrollTop();
-    if (scrollDistance > 100) {
-      $('.scroll-to-top').fadeIn();
-    } else {
-      $('.scroll-to-top').fadeOut();
-    }
-  });
-
-  $(document).on('click', 'a.scroll-to-top', function (e) {
-    $('html, body').stop().animate({
-      scrollTop: 0
-    }, 1000, 'swing');
-    e.preventDefault();
+    window.setupNMSPage();
   });
 }())
