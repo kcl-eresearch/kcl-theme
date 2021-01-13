0dfbf71e12cc8cc8b9f4fd77faa62007e6be635c
Skylar Kelty <skylar.kelty@kcl.ac.uk>
2021-01-13 17:40:03 +0000

    Fix

--- public/assets/js/app-slim.js
+++ public/assets/js/app-slim.js
@@ -14,7 +14,7 @@ global.Bloodhound = Bloodhound;
   require('typeahead.js/dist/typeahead.jquery')
   global.ClipboardJS = require('clipboard')
 
-  require('./common.js?1610558911')
+  require('./common.js?1610559531')
 
   $(document).ready(function() {
     // Quill.
