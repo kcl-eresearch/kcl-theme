aaa1defe2ab97cc109ecc9e1c787dd585c70ac60
Skylar Kelty <skylar.kelty@kcl.ac.uk>
2021-01-13 17:04:21 +0000

    Fix contrast on low-contrast screens and improve visibility for certain sight impairments

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -19,8 +19,8 @@ global.Bloodhound = Bloodhound;
   global.Quill = require('quill')
   require('chart.js')
 
-  require('./common.js?1610535811')
-  require('./chartjs-ext.js?1610535811')
+  require('./common.js?1610557438')
+  require('./chartjs-ext.js?1610557438')
 
   $(document).ready(function() {
     // Quill.
