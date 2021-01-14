a0377fd4ad97afab861bb9afce0f1f0a8ded9015
Skylar Kelty <skylar.kelty@kcl.ac.uk>
2021-01-14 11:24:32 +0000

    Old color scheme making a comeback

--- public/assets/js/app-slim.js
+++ public/assets/js/app-slim.js
@@ -14,7 +14,7 @@ global.Bloodhound = Bloodhound;
   require('typeahead.js/dist/typeahead.jquery')
   global.ClipboardJS = require('clipboard')
 
-  require('./common.js?1610622887')
+  require('./common.js?1610623458')
 
   $(document).ready(function() {
     // Quill.
--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -19,8 +19,8 @@ global.Bloodhound = Bloodhound;
   global.Quill = require('quill')
   require('chart.js')
 
-  require('./common.js?1610622887')
-  require('./chartjs-ext.js?1610622887')
+  require('./common.js?1610623458')
+  require('./chartjs-ext.js?1610623458')
 
   $(document).ready(function() {
     // Quill.
