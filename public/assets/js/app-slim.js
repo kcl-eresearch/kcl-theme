00a34ff686142dfbca7c9cd4b14a1378cb3b902c
Skylar Kelty <skylar.kelty@kcl.ac.uk>
2021-02-26 14:37:18 +0000

    Fixes

--- public/assets/js/app-slim.js
+++ public/assets/js/app-slim.js
@@ -14,7 +14,7 @@ global.Bloodhound = Bloodhound;
   require('typeahead.js/dist/typeahead.jquery')
   global.ClipboardJS = require('clipboard')
 
-  require('./common.js?1613479726')
+  require('./common.js?1614350221')
 
   $(document).ready(function() {
     // Quill.
--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -19,8 +19,8 @@ global.Bloodhound = Bloodhound;
   global.Quill = require('quill')
   require('chart.js')
 
-  require('./common.js?1613479726')
-  require('./chartjs-ext.js?1613479726')
+  require('./common.js?1614350221')
+  require('./chartjs-ext.js?1614350221')
 
   $(document).ready(function() {
     // Quill.
