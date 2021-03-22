d673670fb0b5f1d70e3a6ae79afa7b2510351fae
Skylar Kelty <skylar.kelty@kcl.ac.uk>
2021-03-22 16:13:42 +0000

    More button examples

--- public/assets/js/app-slim.js
+++ public/assets/js/app-slim.js
@@ -14,8 +14,8 @@ global.Bloodhound = Bloodhound;
   require('typeahead.js/dist/typeahead.jquery')
   global.ClipboardJS = require('clipboard')
 
-  require('./common.js?1616429455')
-  require('./animate.js?1616429455')
+  require('./common.js?1616429611')
+  require('./animate.js?1616429611')
 
   $(document).ready(function() {
     // Quill.
