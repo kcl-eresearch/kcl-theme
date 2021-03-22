8252e4c80a7a9021a4e72bb96b5dddaa62eb6818
Skylar Kelty <skylar.kelty@kcl.ac.uk>
2021-03-22 15:47:50 +0000

    Tweaks

--- public/assets/js/app-slim.js
+++ public/assets/js/app-slim.js
@@ -14,8 +14,8 @@ global.Bloodhound = Bloodhound;
   require('typeahead.js/dist/typeahead.jquery')
   global.ClipboardJS = require('clipboard')
 
-  require('./common.js?1616427384')
-  require('./animate.js?1616427384')
+  require('./common.js?1616427798')
+  require('./animate.js?1616427798')
 
   $(document).ready(function() {
     // Quill.
