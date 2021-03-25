c049e50f7b9f758dc5230ffc02b7c72d24ce4027
Skylar Kelty <skylar.kelty@kcl.ac.uk>
2021-03-25 14:38:47 +0000

    Add open source notes

--- public/assets/js/app-slim.js
+++ public/assets/js/app-slim.js
@@ -14,8 +14,8 @@ global.Bloodhound = Bloodhound;
   require('typeahead.js/dist/typeahead.jquery')
   global.ClipboardJS = require('clipboard')
 
-  require('./common.js?1616681940')
-  require('./animate.js?1616681940')
+  require('./common.js?1616683094')
+  require('./animate.js?1616683094')
 
   $(document).ready(function() {
     // Quill.
